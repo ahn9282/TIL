c3a8337598fa16ced5eb880f06014a2d6f5edd0d
cajunSub <junsyub@gmail.com>
2025-01-08 21:52:35 +0900

    종합 공부 프로젝트추가

---
+++ StduyNet/src/main/java/general/StduyNet/StduyNetApplication.java
@@ -0,0 +1,13 @@
+package general.StduyNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StduyNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StduyNetApplication.class, args);
+	}
+
+}
