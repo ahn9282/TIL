a8c7bf3b5792bab4d2b0b9d04d2391e90f962706
cajunSub <junsyub@gmail.com>
2025-02-04 23:58:09 +0900

    토비 스프링 - 3장 템플릿 : UserDao 예외 처리

--- spring/Tobi_spring/vol1/src/main/java/study/user/dao/UserDao.java
+++ spring/Tobi_spring/vol1/src/main/java/study/user/dao/UserDao.java
@@ -3,10 +3,14 @@ package study.user.dao;
 import org.springframework.dao.EmptyResultDataAccessException;
 import study.user.domain.User;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public abstract class UserDao {
     private ConnectionMaker connectionMaker;
+
     public UserDao(ConnectionMaker connectionMaker) {
         this.connectionMaker = connectionMaker;
     }
@@ -35,8 +39,8 @@ public abstract class UserDao {
         ResultSet rs = pstmt.executeQuery();
 
         User user = null;
-        if(rs.next()) {
-             user = new User();
+        if (rs.next()) {
+            user = new User();
             user.setId(rs.getString("id"));
             user.setName(rs.getString("name"));
             user.setPassword(rs.getString("password"));
@@ -45,32 +49,40 @@ public abstract class UserDao {
         pstmt.close();
         con.close();
 
-        if(user == null) throw new EmptyResultDataAccessException(1);
+        if (user == null) throw new EmptyResultDataAccessException(1);
 
         return user;
     }
 
     public void deleteAll() throws SQLException, ClassNotFoundException {
-        Connection con = connectionMaker.makeNewConnection();
-        PreparedStatement pstmt = con.prepareStatement("delete from users");
-        pstmt.executeUpdate();
+        Connection con = null;
+        PreparedStatement pstmt = null;
 
-        pstmt.close();
-        con.close();
+        try {
+            con = connectionMaker.makeNewConnection();
+            pstmt = con.prepareStatement("delete from users");
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (pstmt != null) { try{ pstmt.close();} catch (SQLException e) { } }
+            if (con != null) { try{  con.close();} catch (SQLException e) { } }
+        }
+        }
+
+        public int getCount () throws SQLException, ClassNotFoundException {
+            Connection con = connectionMaker.makeNewConnection();
+            PreparedStatement pstmt = con.prepareStatement("select count(*) from users");
+            ResultSet rs = pstmt.executeQuery();
+            rs.next();
+            int count = rs.getInt(1);
+
+            rs.next();
+            pstmt.close();
+            con.close();
+
+            return count;
+        }
+
     }
-
-    public int getCount() throws SQLException, ClassNotFoundException {
-        Connection con = connectionMaker.makeNewConnection();
-        PreparedStatement pstmt = con.prepareStatement("select count(*) from users");
-        ResultSet rs = pstmt.executeQuery();
-        rs.next();
-        int count = rs.getInt(1);
-
-        rs.next();
-        pstmt.close();
-        con.close();
-
-        return count;
-    }
-
-}
