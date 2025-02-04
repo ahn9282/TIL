bdbe64e8b04cf4097ca605484ca49cb89ceffa57
cajunSub <junsyub@gmail.com>
2025-02-05 00:43:21 +0900

    토비 스프링 - 3장 템플릿 : UserDao 예외 처리 & strategy pattern & nested class

---
+++ spring/Tobi_spring/vol1/src/main/java/study/user/code/StatementStrategy.java
@@ -0,0 +1,9 @@
+package study.user.code;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public interface StatementStrategy {
+    PreparedStatement makePreparedStatement(Connection c) throws SQLException;
+}
--- spring/Tobi_spring/vol1/src/main/java/study/user/dao/UserDao.java
+++ spring/Tobi_spring/vol1/src/main/java/study/user/dao/UserDao.java
@@ -1,6 +1,7 @@
 package study.user.dao;
 
 import org.springframework.dao.EmptyResultDataAccessException;
+import study.user.code.StatementStrategy;
 import study.user.domain.User;
 
 import java.sql.Connection;
@@ -15,21 +16,35 @@ public abstract class UserDao {
         this.connectionMaker = connectionMaker;
     }
 
-    public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection con = connectionMaker.makeNewConnection();
+    public void add(final User user) throws ClassNotFoundException, SQLException {
+        class AddStatement implements StatementStrategy {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement pstmt = c.prepareStatement("insert into users (id, name, password) values (?,?,?)");
+                pstmt.setString(1, user.getId());
+                pstmt.setString(2, user.getName());
+                pstmt.setString(3, user.getPassword());
+                return pstmt;
+            }
+        }
+        AddStatement st = new AddStatement();
+        jdbcContextWithStatementStrategy(st);
 
-        PreparedStatement pstmt = con.prepareStatement("insert into users values (?,?,?)");
-        pstmt.setString(1, user.getId());
-        pstmt.setString(2, user.getName());
-        pstmt.setString(3, user.getPassword());
+    }  public void addLambdaInterClass(final User user) throws ClassNotFoundException, SQLException {
 
-        pstmt.executeUpdate();
-
-        pstmt.close();
-        con.close();
+        StatementStrategy st = new StatementStrategy() {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement pstmt = c.prepareStatement("insert into users (id, name, password) values (?,?,?)");
+                pstmt.setString(1, user.getId());
+                pstmt.setString(2, user.getName());
+                pstmt.setString(3, user.getPassword());
+                return pstmt;
+            }
+        };
+        jdbcContextWithStatementStrategy(st);
 
     }
-
 
     public User get(String id) throws ClassNotFoundException, SQLException {
         Connection con = connectionMaker.makeNewConnection();
@@ -66,23 +81,51 @@ public abstract class UserDao {
         } catch (SQLException e) {
             throw e;
         } finally {
-            if (pstmt != null) { try{ pstmt.close();} catch (SQLException e) { } }
-            if (con != null) { try{  con.close();} catch (SQLException e) { } }
+            if (pstmt != null) { try { pstmt.close();  } catch (SQLException e) {  } }
+            if (con != null) { try {  con.close(); } catch (SQLException e) { }}
         }
-        }
-
-        public int getCount () throws SQLException, ClassNotFoundException {
-            Connection con = connectionMaker.makeNewConnection();
-            PreparedStatement pstmt = con.prepareStatement("select count(*) from users");
-            ResultSet rs = pstmt.executeQuery();
-            rs.next();
-            int count = rs.getInt(1);
-
-            rs.next();
-            pstmt.close();
-            con.close();
-
-            return count;
-        }
-
     }
+    public void deleteAllLambdaInnerClass() throws SQLException, ClassNotFoundException {
+        StatementStrategy st = new StatementStrategy() {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                return c.prepareStatement("delete from users");
+            }
+        };
+        jdbcContextWithStatementStrategy(st);
+    }
+
+    public int getCount() throws SQLException, ClassNotFoundException {
+        Connection con = connectionMaker.makeNewConnection();
+        PreparedStatement pstmt = con.prepareStatement("select count(*) from users");
+        ResultSet rs = pstmt.executeQuery();
+        rs.next();
+        int count = rs.getInt(1);
+
+        rs.next();
+        pstmt.close();
+        con.close();
+
+        return count;
+    }
+
+    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException{
+        Connection con =null;
+        PreparedStatement pstmt = null;
+
+        try{
+            con = connectionMaker.makeNewConnection();
+            pstmt = stmt.makePreparedStatement(con);
+
+            pstmt.executeUpdate();
+        }catch(SQLException e){
+            throw e;
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally{
+            if (pstmt != null) { try { pstmt.close();  } catch (SQLException e) {  } }
+            if (con != null) { try {  con.close(); } catch (SQLException e) { }}
+        }
+    }
+
+}
