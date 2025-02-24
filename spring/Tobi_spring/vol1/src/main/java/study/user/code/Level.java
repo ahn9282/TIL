a4f1e52917f8665e5cd03dc63489bb620d30e399
cajunSub <junsyub@gmail.com>
2025-02-25 00:25:31 +0900

    토비 스프링 - 5장 서비스 책임의 분리 : 코드 개선

--- spring/Tobi_spring/vol1/src/main/java/study/user/code/Level.java
+++ spring/Tobi_spring/vol1/src/main/java/study/user/code/Level.java
@@ -7,11 +7,20 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum Level {
-    BASIC(1),
-    SILVER(2),
-    GOLD(3),
+    GOLD(3, null),
+    SILVER(2, GOLD),
+    BASIC(1, SILVER),
     ;
     private final int value;
+    private Level next;
+
+    public int intValue(){
+        return value;
+    }
+    public Level nextLevel(){
+        return this.next;
+    }
+
     public static Level valueOf(int value){
         switch(value){
             case 1: return BASIC;
