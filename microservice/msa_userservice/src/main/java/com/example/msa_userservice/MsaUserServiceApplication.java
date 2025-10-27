d5fe4c0001008c88e762da34f19490ae1b4ef94f
jsa <junsyub@gmail.com>
2025-10-28 00:24:25 +0900

    mas - add userservice

--- microservice/msa_userservice/src/main/java/com/example/msa_userservice/MsaUserServiceApplication.java
+++ microservice/msa_userservice/src/main/java/com/example/msa_userservice/MsaUserServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MsaUserserviceApplication {
+public class MsaUserServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MsaUserserviceApplication.class, args);
+        SpringApplication.run(MsaUserServiceApplication.class, args);
     }
 
 }
