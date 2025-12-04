8bd6bba3378842692e69b19302160371e5724e58
jsa <junsyub@gmail.com>
2025-12-04 23:56:41 +0900

    test study - create project & start study

---
+++ test-ajs/src/main/java/com/ajs/test/study/TestAjsApplication.java
@@ -0,0 +1,13 @@
+package com.ajs.test.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAjsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestAjsApplication.class, args);
+    }
+
+}
---
+++ test-ajs/src/test/java/com/ajs/test/study/TestAjsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajs.test.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAjsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
