5d61c85460ff7e590f0162e54292f6860849c3e3
jsa <junsyub@gmail.com>
2025-12-09 00:11:21 +0900

    test study - chapter2 mock test - 성적 저장 로직 테스트

--- test-ajs/src/test/java/com/ajs/test/study/chapter2_mockito/service/StudentServiceTest.java
+++ test-ajs/src/test/java/com/ajs/test/study/chapter2_mockito/service/StudentServiceTest.java
@@ -23,5 +23,60 @@ class StudentServiceTest {
         studentScoreService.saveScore("준섭", Exam.math, 80, 70, 55);
 
     }
+
+    @Test
+    @DisplayName("성적 저장 로직 검증")
+    public void saveScoreMockTest() {
+        StudentScoreRepository studentScoreRepository = Mockito.mock(StudentScoreRepository.class);
+        StudentPassRepository studentPassRepository = Mockito.mock(StudentPassRepository.class);
+        StudentFailRepository studentFailRepository = Mockito.mock(StudentFailRepository.class);
+
+        StudentService studentService = new StudentService(
+                studentScoreRepository
+                , studentPassRepository
+                , studentFailRepository
+        );
+        String givenStudentName = "jsa";
+        Exam givenExam = Exam.math;
+        Integer givenKorScore = 80;
+        Integer givenEngScore = 70;
+        Integer givenMathScore = 60;
+
+        studentService.saveScore(
+                givenStudentName
+                , givenExam
+                , givenKorScore
+                , givenMathScore
+                , givenEngScore
+        );
+
+        Mockito.verify(studentScoreRepository, Mockito.times(1)).save(Mockito.any());
+        Mockito.verify(studentPassRepository, Mockito.times(1)).save(Mockito.any());
+        Mockito.verify(studentFailRepository, Mockito.times(0)).save(Mockito.any());
+    }
+
+    @Test
+    @DisplayName("성적 저장 60 점 미만인 경우")
+    public void saveScoreFailTest(){
+        StudentScoreRepository studentScoreRepository = Mockito.mock(StudentScoreRepository.class);
+        StudentPassRepository studentPassRepository = Mockito.mock(StudentPassRepository.class);
+        StudentFailRepository studentFailRepository = Mockito.mock(StudentFailRepository.class);
+
+        StudentService studentService = new StudentService(
+                studentScoreRepository
+                , studentPassRepository
+                , studentFailRepository
+        );
+        String givenStudentName = "jsa";
+        Exam givenExam = Exam.math;
+        Integer givenKorScore = 60;
+        Integer givenEngScore = 40;
+        Integer givenMathScore = 55;
+        studentService.saveScore(givenStudentName, givenExam, givenKorScore, givenMathScore, givenEngScore);
+
+        Mockito.verify(studentScoreRepository, Mockito.times(1)).save(Mockito.any());
+        Mockito.verify(studentPassRepository, Mockito.times(0)).save(Mockito.any());
+        Mockito.verify(studentFailRepository, Mockito.times(1)).save(Mockito.any());
+    }
 }
 
