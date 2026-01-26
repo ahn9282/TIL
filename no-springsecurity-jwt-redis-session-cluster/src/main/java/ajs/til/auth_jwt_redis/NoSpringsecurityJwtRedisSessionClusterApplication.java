16492e5e09fd376560cd0249695f91142286ebd9
jsa <junsyub@gmail.com>
2026-01-27 00:38:27 +0900

    jwt&redis - session clustering : crate project

---
+++ no-springsecurity-jwt-redis-session-cluster/src/main/java/ajs/til/auth_jwt_redis/NoSpringsecurityJwtRedisSessionClusterApplication.java
@@ -0,0 +1,13 @@
+package ajs.til.auth_jwt_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoSpringsecurityJwtRedisSessionClusterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoSpringsecurityJwtRedisSessionClusterApplication.class, args);
+    }
+
+}
---
+++ no-springsecurity-jwt-redis-session-cluster/src/test/java/ajs/til/auth_jwt_redis/NoSpringsecurityJwtRedisSessionClusterApplicationTests.java
@@ -0,0 +1,13 @@
+package ajs.til.auth_jwt_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoSpringsecurityJwtRedisSessionClusterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
