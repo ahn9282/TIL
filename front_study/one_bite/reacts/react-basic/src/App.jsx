e872cf6483973b6fc39587d9f4edd2c6b878ad7c
jsa <junsyub@gmail.com>
2026-04-02 01:18:23 +0900

    React - Component (basic)

--- front_study/one_bite/reacts/react-basic/src/App.jsx
+++ front_study/one_bite/reacts/react-basic/src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+
+
 
 function App() {
 
   return (
     <>
-      <h1>Wecome React!!</h1>
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
