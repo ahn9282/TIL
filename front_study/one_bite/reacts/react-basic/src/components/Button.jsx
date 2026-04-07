c6c18dfb7d312a52313696407b87f3e76c4cefa8
jsa <junsyub@gmail.com>
2026-04-08 00:34:21 +0900

    React - event handling

--- front_study/one_bite/reacts/react-basic/src/components/Button.jsx
+++ front_study/one_bite/reacts/react-basic/src/components/Button.jsx
@@ -1,10 +1,18 @@
 const Button = ({text, color="black", children}) => {
 
-    const onClickHandler = () => {
+    const onClickHandler = (e) => {
         console.log({text, color});
+        console.log(e);
     }  
     return (
-        <button onClick={onClickHandler} style={{ backgroundColor: color }}>
+        <button 
+            onClick={onClickHandler} 
+            onMouseEnter={() => {console.log(text   + "에 마우스가 올라갔습니다.")}}
+            style={{ 
+                backgroundColor: color, 
+                color: 'white',
+            }}
+        >
             {text}
             {children}
         </button>
