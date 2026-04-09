22aa6074d86521563317630783ce7f858353b7ce
jsa <junsyub@gmail.com>
2026-04-10 00:59:18 +0900

    React - simple counter - state lifting

--- front_study/one_bite/reacts/simple-counter-app/src/App.jsx
+++ front_study/one_bite/reacts/simple-counter-app/src/App.jsx
@@ -1,12 +1,27 @@
 import './App.css'
+import Viewer from './components/Viewer.jsx'
+import Controller from './components/Controller.jsx'
+import {useState} from "react";
 
 function App() {
 
-  return (
-    <>
-      카운터앱
-    </>
-  )
+    const [count, setCount] = useState(0);
+
+    const onClickBtn = function(value){
+        setCount(count + value);
+    }
+
+    return (
+        <div className="App">
+            <h1>Simple Counter App</h1>
+            <section>
+                <Viewer count={count}/>
+            </section>
+            <section>
+                <Controller onClickBtn={onClickBtn}/>
+            </section>
+        </div>
+    )
 }
 
 export default App
